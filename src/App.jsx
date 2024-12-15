93093eb4f610a5b91c53fb3a4d9d643fb8ad25a9
neupanesaugat <saugatneupane50@gmail.com>
2024-12-15 23:11:04 +0545

    mui-intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Person from "./components/counter/Person";
+import Animal from "./components/counter/animal";
 
 const App = () => {
   return (
     <div style={{ display: "flex", marginBlockEnd: "2rem", flexWrap: "wrap" }}>
-      <Person />
+      <Animal />
     </div>
   );
 };
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,1 +1,4 @@
 export const getFullName = (firstName, lastName) => `${firstName} ${lastName}`;
+
+export const getAnimalName = (animalName, animalType) =>
+  `${animalName} is a ${animalType} animal`;
